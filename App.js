b444282ce2192424f072f1aec70190df998c279b
2neha <neha.hrwosit1019@gmail.com>
2020-10-22 23:05:16 +0530

    ready to commit

---
+++ App.js
@@ -0,0 +1,98 @@
+
+/*Example of SQLite Database in React Native*/
+import React from 'react';
+
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+
+import Header from './src/Header';
+import Mission from './src/Mission';
+
+
+const App = createStackNavigator({
+  Header: {
+    screen: Header,
+  
+  },
+  Mission: {
+    screen: Mission ,
+  
+  },
+ 
+});
+export default createAppContainer(App);
+// import React from 'react';
+// import { StyleSheet, Text, View ,Image,TouchableOpacity,Button,Icon} from 'react-native';
+// import { ScrollView } from 'react-native-gesture-handler';
+// import Reg from './src/Reg';
+//  import Services from './src/Services';
+// // import  FormDialog from './src/Form';
+// import Header from './src/Header';
+// import Swipe from './src/Swipe';
+// import Book from './src/Book';
+// import Order from './src/Order';
+// // import Login from './src/Login';
+// //  import sp from './assets/sp.jpg'; 
+// //  import main from './assets/main.png'; 
+// import Foot from './src/Foot';
+
+ 
+// export default class App  extends React.Component {
+//   render() {
+//     return (
+      
+//       <View style={{flex:1}}>
+
+//          <Header/>
+//          <View style={{flexDirection:'row' }}>
+        
+
+//          <Swipe/>
+//          <View style={{justifyContent:'center',alignItems:'center' ,flex:1 }}>
+//          <Reg/>
+//          </View>
+        
+//          </View>
+//          <View> 
+//           <Book/>
+//         </View>
+//         <View> 
+//           <Order/>
+//         </View>
+//         {/* <View>
+//           < FormDialog/>
+//         </View> */}
+//         <View>
+//           <Services/>
+//         </View>
+//         <View style={{backgroundColor:'#1f2d61'}} >
+//           <Foot/>
+
+//         </View>
+//         {/* <View>
+//           <Bottom/>
+//         </View> */}
+// {/*          
+
+//          <Signup/>
+//          <Login/> */}
+        
+        
+    
+    
+         
+    
+//       <ScrollView>
+ 
+
+//       </ScrollView>
+      
+              
+   
+    // 
+
+//   </View>
+//     );
+//   }
+//  }
