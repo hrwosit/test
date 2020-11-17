9919905c14069070ab567b34fb1ac2df056ed134
priyankamane07 <priyankamane234@gmail.com>
2020-11-17 22:23:39 +0530

    added constants.js glondhe

---
+++ src/common/constants.js
@@ -0,0 +1,7 @@
+// export const URI_BASE = "http://192.168.43.118:8000/"; //ganesh
+export const URI_BASE = "localhost:8000/"; //for developers use only
+
+// authentication links
+export const URI_AUTH_REGISTER = URI_BASE + "auth/register/";
+export const URI_AUTH_LOGIN = URI_BASE + "auth/login/";
+
